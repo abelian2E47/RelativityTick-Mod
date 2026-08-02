05978ac0b017504c930f47f5d152bba7870f6ce1
Ay2E47 <3323141682@QQ.com>
2026-08-02 12:43:35 +0800

    修复区域边框世界坐标渲染

--- src/client/java/com/abelian/client/mixin/WorldRendererMixin.java
+++ src/client/java/com/abelian/client/mixin/WorldRendererMixin.java
@@ -2,16 +2,12 @@ package com.abelian.client.mixin;
 
 import com.abelian.client.clientRegionTick.ClientRegion;
 import com.abelian.client.clientRegionTick.ClientRegionManager;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.*;
+import net.minecraft.client.render.debug.DebugRenderer;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.util.math.Vec3d;
-import net.minecraft.util.profiler.Profiler;
-import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 
 import static com.abelian.client.render.RendererUtils.renderChunkLines;
@@ -19,54 +15,48 @@ import static com.abelian.client.RelativityTickClient.selectChunks;
 
 @Mixin(WorldRenderer.class)
 public abstract class WorldRendererMixin {
-    @Inject(
+    @Redirect(
             method = "render",
-            at = @At("TAIL")
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/render/debug/DebugRenderer;render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider$Immediate;DDD)V"
+            )
     )
     private void renderRegionBoundaries(
-            RenderTickCounter tickCounter,
-            boolean tick,
-            Camera camera,
-            GameRenderer gameRenderer,
-            LightmapTextureManager lightmapTextureManager,
-            Matrix4f positionMatrix,
-            Matrix4f projectionMatrix,
-            CallbackInfo ci) {
+            DebugRenderer debugRenderer,
+            MatrixStack matrices,
+            VertexConsumerProvider.Immediate consumers,
+            double cameraX,
+            double cameraY,
+            double cameraZ
+    ) {
+        debugRenderer.render(matrices, consumers, cameraX, cameraY, cameraZ);
         if (selectChunks.isEmpty() && ClientRegionManager.getRegions().isEmpty()) return;
 
-        Vec3d camPos = camera.getPos();
-        MatrixStack matrices = new MatrixStack();
-        matrices.translate(-camPos.x, -camPos.y, -camPos.z);
-
-        VertexConsumerProvider.Immediate consumers = MinecraftClient.getInstance().getBufferBuilders().getEntityVertexConsumers();
+        matrices.push();
+        matrices.translate(-cameraX, -cameraY, -cameraZ);
         VertexConsumer lineConsumer = consumers.getBuffer(RenderLayer.getDebugLineStrip(5.0));
-        //渲染选择中的区块
-        if (!selectChunks.isEmpty()){
+
+        if (!selectChunks.isEmpty()) {
             float r1 = 1.0F, g1 = 0.5F, b1 = 0.0F;
             for (long posLong : selectChunks) {
                 renderChunkLines(matrices, lineConsumer, posLong, -64, 320, r1, g1, b1, selectChunks);
             }
         }
 
-
-        //渲染已选择的区域
         for (ClientRegion region : ClientRegionManager.getRegions()) {
             if (region.getChunkPositions() == null || region.getDimension().isEmpty()) continue;
 
             float r2, g2, b2;
-
             if (region.isRunning()) {
-                //紫色
                 r2 = 0.8f;
                 g2 = 0.2f;
                 b2 = 1.0f;
             } else if (region.isControlled()) {
-                //蓝色
                 r2 = 0.0f;
                 g2 = 0.5f;
                 b2 = 1.0f;
             } else {
-                //绿色
                 r2 = 0.0f;
                 g2 = 1.0f;
                 b2 = 0.0f;
@@ -76,7 +66,7 @@ public abstract class WorldRendererMixin {
                 renderChunkLines(matrices, lineConsumer, posLong, -64, 320, r2, g2, b2, region.getChunkPositions());
             }
         }
+        matrices.pop();
     }
-
 }
 
