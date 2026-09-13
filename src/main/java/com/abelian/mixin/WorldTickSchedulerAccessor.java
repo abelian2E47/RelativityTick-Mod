ded084104ecf0bf4fcb846655c7bd00192d3c4c0
Ay2E47 <3323141682@QQ.com>
2026-09-13 12:04:21 +0800

    修复错误的计划刻行为

--- src/main/java/com/abelian/mixin/WorldTickSchedulerAccessor.java
+++ src/main/java/com/abelian/mixin/WorldTickSchedulerAccessor.java
@@ -8,6 +8,9 @@ import net.minecraft.world.tick.WorldTickScheduler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
 import java.util.function.BiConsumer;
 
 @Mixin(WorldTickScheduler.class)
@@ -20,4 +23,15 @@ public interface WorldTickSchedulerAccessor<T> {
 
     @Accessor("queuedTickConsumer")
     BiConsumer<ChunkTickScheduler<T>, OrderedTick<T>> getQueuedTickConsumer();
+
+    //本刻"已出队、待执行/正在执行"的整批计划刻。isTicking() 只看这三个容器,
+    //区域自管计划刻时必须往这里写,否则 isTicking() 恒为 false。
+    @Accessor("tickableTicks")
+    Queue<OrderedTick<T>> getTickableTicks();
+
+    @Accessor("tickedTicks")
+    List<OrderedTick<T>> getTickedTicks();
+
+    @Accessor("copiedTickableTicksList")
+    Set<OrderedTick<?>> getCopiedTickableTicksList();
 }
